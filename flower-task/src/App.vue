9916b8b417d394fbd14d8135a17a3695a2e4c285
lunaskristoffersen <lunaskristoffersen@gmail.com>
2026-09-10 14:09:58 +0200

    content

---
+++ flower-task/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts"></script>
+
+<template style= "width=100%">
+  <div class="rectangle"></div>
+    
+</template>
+
+<style>
+.rectangle {
+  display: flex;
+  position: fixed;
+  height: 120px;
+  width: 100%;
+  bottom: 0;
+  background-color: #947158;
+  margin: 0;
+  padding: 0;
+  z-index: -1;
+}
+
+html {
+  width: 100%;
+}
+
+body {
+  width: 100%;
+  margin: 0;
+}
+</style>
